6745862ad3372467ae6b2cb91d469ab2bff09250
rza101 <69953361+rza101@users.noreply.github.com>
2023-09-06 21:37:52 +0700

    reformat code

--- app/src/main/java/com/rhezarijaya/storiesone/data/network/service/StoryAPIService.kt
+++ app/src/main/java/com/rhezarijaya/storiesone/data/network/service/StoryAPIService.kt
@@ -2,7 +2,6 @@ package com.rhezarijaya.storiesone.data.network.service
 
 import com.rhezarijaya.storiesone.data.network.response.BaseResponse
 import com.rhezarijaya.storiesone.data.network.response.LoginResponse
-import com.rhezarijaya.storiesone.data.network.response.StoryDetailResponse
 import com.rhezarijaya.storiesone.data.network.response.StoryListResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -12,7 +11,6 @@ import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface StoryAPIService {
--- app/src/main/java/com/rhezarijaya/storiesone/util/Result.kt
+++ app/src/main/java/com/rhezarijaya/storiesone/util/Result.kt
@@ -1,7 +1,5 @@
 package com.rhezarijaya.storiesone.util
 
-import java.lang.Exception
-
 sealed class Result<out R> private constructor() {
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
